b7b2061f3c89055c5bc0414e159b8add25251012
dor-co <dor713cohen@gmail.com>
2021-07-27 01:40:17 +0300

    delete button with asking - done

--- src/components/movieCard/MovieCard.js
+++ src/components/movieCard/MovieCard.js
@@ -1,15 +1,12 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { open } from "../../redux/Actions";
 import './Style.css';
 import { Button } from "react-bootstrap";
-import { useFirestore } from "reactfire";
 import * as MdIcons from 'react-icons/md';
 
 function MovieCard({ item }) {
 
     const dispatch = useDispatch();
-
-    const db = useFirestore();
 
     const openMovieDetails = (val, movieDetailsBody, title, category, rate, id, image) => {
         dispatch(open(val, movieDetailsBody, title, category, rate, id, image));
@@ -24,14 +21,13 @@ function MovieCard({ item }) {
     }
 
     const deleteMovie = () => {
-        db.collection("Movies").doc(item.id)
-        .delete()
+        openMovieDetails('Delete Movie', 'delete', item.title, item.category, item.rate, item.id, item.imageSrc)
     }
 
     return (
         <>
-            <div onClick={viewMovieDetails} className="card border-dark mb-3" style={{ width: '80%', height: 100, margin: 'auto', border: 'none', borderRadius: 0, borderBottom: '2px solid' }}>
-                <div className="card-body text-dark" style={{ display: 'flex' }}>
+            <div onClick={viewMovieDetails} className="card border-dark mb-3 cardTextStyle">
+                <div className="card-body text-dark cardTextBody">
                     <img className='movieImg' src={item.imageSrc} />
                     <h6 className='movieTitle'>{item.title}</h6>
                 </div>
--- src/components/movieDetailsBody/MovieDetailsBody.js
+++ src/components/movieDetailsBody/MovieDetailsBody.js
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
-import { useState } from "react";
 import { useFirestore } from "reactfire";
 import { useDispatch } from "react-redux";
 import { close } from "../../redux/Actions";
@@ -12,18 +11,38 @@ function MovieDetailsBody() {
 
     const dispatch = useDispatch();
 
+    const db = useFirestore();
+
     const closeModal = () => {
+        return dispatch(close())
+    }
+
+    const clickDeleteMovie = () => {
+        db.collection("Movies").doc(moviesRed.id)
+        .delete()
+
         return dispatch(close())
     }
 
     return (
         <div>
-            <h2>{moviesRed.title}</h2>
-            <h5><MdIcons.MdStar size={25} color={'#eccf00'} /> {moviesRed.rate}/10</h5>
-            <h6>{moviesRed.category}</h6>
+            {moviesRed.body === 'no add' ? (
+                <>
+                    <h2>{moviesRed.title}</h2>
+                    <h5><MdIcons.MdStar size={25} color={'#eccf00'} /> {moviesRed.rate}/10</h5>
+                    <h6>{moviesRed.category}</h6>
+                </>
+            ) : (
+                <>
+                    <h4 className='deleteTitle'>Are you sure you want to delete '{moviesRed.title}' movie?</h4>
+                </>
+            )}
 
             <div>
-                <Button className='closeModalBtn' onClick={closeModal}>Close</Button>
+                {moviesRed.body === 'delete' && 
+                    <Button className='deleteMovieBtn' onClick={clickDeleteMovie}>Delete</Button>                
+                }
+                <Button className='closeModalBtn' onClick={closeModal}>{moviesRed.body === 'no add' ? 'Close' : 'Cancel'}</Button>
             </div>
         </div>
     );
